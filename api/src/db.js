635a8d199a8a82ed0fa7ffb6df8317cec1d80f8e
MaxiCassol <maximilianocassol@gmail.com>
2023-06-04 19:31:39 -0300

    deploy3

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,8 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_DEPLOY
+  // DB_USER, DB_PASSWORD, DB_HOST,
+  DB_DEPLOY
 } = process.env;
 
 // const modelGenres = require("./models/Genres");
