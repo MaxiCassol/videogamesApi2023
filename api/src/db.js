ff2f210c18a63541b967f2326aa25510adb0d256
Maximiliano Cassol <115405448+MaxiCassol@users.noreply.github.com>
2023-07-12 14:11:19 -0300

    profile listo

--- api/src/db.js
+++ api/src/db.js
@@ -3,8 +3,8 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  // DB_USER, DB_PASSWORD, DB_HOST,
-  DB_DEPLOY
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  
 } = process.env;
 
 // const modelGenres = require("./models/Genres");
@@ -16,7 +16,7 @@ const {
 // });
 // const basename = path.basename(__filename);
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
